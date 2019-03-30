6e21fa4361497356eb2d4c69b3e66e7f93598dd7
Eric Underwood <e.b.underwood@gmail.com>
2019-03-30 16:26:52 -0700

    Added class frameworks (again)...

---
+++ src/FrobWorld.java
@@ -0,0 +1,9 @@
+
+public class FrobWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Thing.java
@@ -0,0 +1,36 @@
+
+public class Thing {
+	// Members
+	private int x;
+	private int y;
+	private char type;
+	
+	// Constructor
+	public Thing ( int x, int y, char type ) {
+		this.x = x;
+		this.y = y;
+		this.type = 'e';
+	}
+	
+	// Getters
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+
+	public char getType() {
+		return type;
+	}
+	
+	// Setters
+	public void setX( int x ) {
+		this.x = x;
+	}
+	
+	public void setY( int y ) {
+		this.y = y;
+	}
+}
